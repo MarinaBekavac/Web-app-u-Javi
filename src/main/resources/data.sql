9e7e509d5f6f935503f336944226be3c63bec5e1
Marina Bekavac <marina.bekavac95@gmail.com>
2022-05-16 20:32:14 +0200

    Addad data to DB

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,3 +8,21 @@ insert into review values(3, 2, 'Third review', 'This is a third review', 'three
 insert into review values(4, 3, 'Fourth review', 'This is a fourth review', 'three_stars');
 insert into review values(5, 3, 'Fifth review', 'This is a fifth review', 'four_stars');
 insert into review values(6, 3, 'Sixth review', 'This is a sixth review', 'one_star');
+
+
+insert into user(id, username, password)
+values
+    (1, 'user', '$2a$12$h0HcS2QDb/7zPASbLa2GoOTSRP6CWK0oX7pCK.dPjkM6L5N4pNovi'), -- password = user
+    (2, 'admin', '$2a$12$INo0nbj40sQrTB7b28KJput/bNltGmFyCfRsUhvy73qcXo5/XdsTG'); -- password = admin
+
+
+insert into authority (id, authority_name)
+values
+    (1, 'ROLE_ADMIN'),
+    (2, 'ROLE_USER');
+
+
+insert into user_authority (user_id, authority_id)
+values
+    (1, 2),
+    (2, 1);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -19,3 +19,19 @@ create table review(
     rating varchar(50) not null,
     foreign key (hardware_id) references hardware(id)
 );
+
+create table if not exists user (
+    id identity,
+    username varchar(100) not null unique,
+    password varchar(1000) not null
+    );
+create table if not exists authority (
+     id identity,
+     authority_name varchar(100) not null unique
+    );
+create table if not exists user_authority (
+    user_id bigint not null,
+    authority_id bigint not null,
+    constraint fk_user foreign key (user_id) references user(id),
+    constraint fk_authority foreign key (authority_id) references authority(id)
+    );
